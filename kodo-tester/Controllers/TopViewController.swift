aeb501b4722d7862d7058390ea6b2abd4ecd31d2
Shota Kurihara <shotath911@gmail.com>
2015-08-02 03:03:50 +0900

    #1 画面遷移のベースを作成

--- kodo-tester/Controllers/TopViewController.swift
+++ kodo-tester/Controllers/TopViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TopViewController.swift
 //  kodo-tester
 //
 //  Created by shota_th on 2015/08/02.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TopViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
